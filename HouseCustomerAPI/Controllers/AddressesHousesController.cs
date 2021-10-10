1d947b8f6e14396cf37bda668fe58aff47a3565f
Demidron <andrew.demidyuk@gmail.com>
2021-10-10 16:55:21 +0300

    Add controllers

---
+++ HouseCustomerAPI/Controllers/AddressesHousesController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HouseCustomerAPI.Models;
+using HouseCustomerAPI.PresentationModels;
+
+namespace HouseCustomerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressesHousesController : ControllerBase
+    {
+        private readonly HouseConsumersDBContext _context;
+
+        public AddressesHousesController(HouseConsumersDBContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/StreetHouses
+        [Route("StreetHouses")]
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<HouseNumberDTO>>> GetAllHousesOnStreet(StreetDTO streetDTO)
+        {
+            var strs = await _context.AddressesHouses.Where(x=>x.StreetId==streetDTO.StreetId&&x.TypeId==streetDTO.TypeStreetId)
+                .Select(a=>new HouseNumberDTO { Id=a.Id, HouseNumber=a.HouseNumber}).ToListAsync();
+
+            return strs;
+        }
+        // GET: api/Streets
+        [Route("Streets")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StreetDTO>>> GetAllHouseStreets()
+        {
+            var strs = await _context.AddressesHouses
+                .OrderBy(x => x.Street.StreetName)
+                .Select(x=>new StreetDTO { StreetId = x.StreetId, TypeStreetId = x.TypeId, StreetName = x.Type.StreetTypeName + ' ' + x.Street.StreetName })
+                .Distinct()
+                .ToListAsync();
+
+            return strs;
+        }
+    
+
+
+
+        // GET: api/AddressesHouses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AddressesHouse>>> GetAddressesHouses()
+        {
+            return await _context.AddressesHouses.ToListAsync();
+        }
+
+        // GET: api/AddressesHouses/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AddressesHouse>> GetAddressesHouse(int id)
+        {
+            var addressesHouse = await _context.AddressesHouses.FindAsync(id);
+
+            if (addressesHouse == null)
+            {
+                return NotFound();
+            }
+
+            return addressesHouse;
+        }
+
+        // PUT: api/AddressesHouses/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAddressesHouse(int id, AddressesHouse addressesHouse)
+        {
+            if (id != addressesHouse.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(addressesHouse).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AddressesHouseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/AddressesHouses
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<AddressesHouse>> PostAddressesHouse(AddressesHouse addressesHouse)
+        {
+            _context.AddressesHouses.Add(addressesHouse);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAddressesHouse", new { id = addressesHouse.Id }, addressesHouse);
+        }
+
+        // DELETE: api/AddressesHouses/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAddressesHouse(int id)
+        {
+            var addressesHouse = await _context.AddressesHouses.FindAsync(id);
+            if (addressesHouse == null)
+            {
+                return NotFound();
+            }
+
+            _context.AddressesHouses.Remove(addressesHouse);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AddressesHouseExists(int id)
+        {
+            return _context.AddressesHouses.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ HouseCustomerAPI/Controllers/ApartmentsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HouseCustomerAPI.Models;
+using HouseCustomerAPI.PresentationModels;
+
+namespace HouseCustomerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApartmentsController : ControllerBase
+    {
+        private readonly HouseConsumersDBContext _context;
+
+        public ApartmentsController(HouseConsumersDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/HouseApartments
+        [Route("HouseApartments")]
+        [HttpGet]////////////////////////////////////////////////////не могу написать   [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<ApartmentNumberDTO>>> GetHouseApartments(int houseId)
+        {
+            var apartments = await _context.Apartments.Where(ap => ap.AddressHouseId == houseId)
+                .Select(x => new ApartmentNumberDTO { Id = x.Id, ApartmentNumber = x.ApartmentNumber })
+                .ToListAsync();
+            return apartments;
+        }
+
+        // GET: api/Apartments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Apartment>>> GetApartments()
+        {
+            return await _context.Apartments.ToListAsync();
+        }
+
+        // GET: api/Apartments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Apartment>> GetApartment(int id)
+        {
+            var apartment = await _context.Apartments.FindAsync(id);
+
+            if (apartment == null)
+            {
+                return NotFound();
+            }
+
+            return apartment;
+        }
+
+        // PUT: api/Apartments/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutApartment(int id, Apartment apartment)
+        {
+            if (id != apartment.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(apartment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ApartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Apartments
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Apartment>> PostApartment(Apartment apartment)
+        {
+            _context.Apartments.Add(apartment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetApartment", new { id = apartment.Id }, apartment);
+        }
+
+        // DELETE: api/Apartments/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteApartment(int id)
+        {
+            var apartment = await _context.Apartments.FindAsync(id);
+            if (apartment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Apartments.Remove(apartment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ApartmentExists(int id)
+        {
+            return _context.Apartments.Any(e => e.Id == id);
+        }
+    }
+}
