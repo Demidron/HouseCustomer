e2bc45d1c9ba40217f7cf0f64121654351b22e70
Demidron <andrew.demidyuk@gmail.com>
2021-10-25 23:44:08 +0300

    Add ConsumersApartments and HotWaterReadings controllers

---
+++ HouseCustomerAPI/Controllers/ConsumersApartmentsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HouseCustomerAPI.Models;
+using HouseCustomerAPI.PresentationModels;
+
+namespace HouseCustomerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsumersApartmentsController : ControllerBase
+    {
+        private readonly HouseConsumersDBContext _context;
+
+        public ConsumersApartmentsController(HouseConsumersDBContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        [Route("ApartmentssssByConsumerId/{id:int}")]
+        public async Task<ActionResult<IEnumerable<ApartmentDTO>>> GetApartmentsByConsumerId(int id)
+        {
+            var consumersApartment = await _context.ConsumersApartments.Where(x => x.ConsumerId == id).Select(conap => new ApartmentDTO
+            {
+                ApartmentId = conap.Apartment.Id,
+                ApartmentNumber = conap.Apartment.ApartmentNumber,
+                FullAdress = conap.Apartment.AddressHouse.Type.StreetTypeName + " " +
+                            conap.Apartment.AddressHouse.Street.StreetName + " " +
+                            conap.Apartment.AddressHouse.HouseNumber
+            }).ToListAsync();
+
+            return consumersApartment;
+        }
+
+        // GET: api/ConsumersApartments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ConsumersApartment>>> GetConsumersApartments()
+        {
+            return await _context.ConsumersApartments.ToListAsync();
+        }
+
+        // GET: api/ConsumersApartments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ConsumersApartment>> GetConsumersApartment(int id)
+        {
+            var consumersApartment = await _context.ConsumersApartments.FindAsync(id);
+
+            if (consumersApartment == null)
+            {
+                return NotFound();
+            }
+
+            return consumersApartment;
+        }
+
+        // PUT: api/ConsumersApartments/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutConsumersApartment(int id, ConsumersApartment consumersApartment)
+        {
+            if (id != consumersApartment.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(consumersApartment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ConsumersApartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ConsumersApartments
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ConsumersApartment>> PostConsumersApartment(ConsumersApartment consumersApartment)
+        {
+            _context.ConsumersApartments.Add(consumersApartment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetConsumersApartment", new { id = consumersApartment.Id }, consumersApartment);
+        }
+
+        // DELETE: api/ConsumersApartments/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteConsumersApartment(int id)
+        {
+            var consumersApartment = await _context.ConsumersApartments.FindAsync(id);
+            if (consumersApartment == null)
+            {
+                return NotFound();
+            }
+
+            _context.ConsumersApartments.Remove(consumersApartment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ConsumersApartmentExists(int id)
+        {
+            return _context.ConsumersApartments.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ HouseCustomerAPI/Controllers/HotWaterReadingsController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HouseCustomerAPI.Models;
+
+namespace HouseCustomerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotWaterReadingsController : ControllerBase
+    {
+        private readonly HouseConsumersDBContext _context;
+
+        public HotWaterReadingsController(HouseConsumersDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("HotWaterReadingsByApartmentId/{id:int}")]
+        public async Task<ActionResult<HotWaterReading>> GetLastHotWaterReadingByApartmentId(int id)
+        {
+            return await _context.HotWaterReadings
+                .Where(x=>x.ApartmentId==id)
+                .OrderByDescending(x => x.CurrentReadingsDate)
+                .FirstAsync();
+        }
+
+        // GET: api/HotWaterReadings
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<HotWaterReading>>> GetHotWaterReadings()
+        {
+            return await _context.HotWaterReadings.ToListAsync();
+        }
+
+        // GET: api/HotWaterReadings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<HotWaterReading>> GetHotWaterReading(int id)
+        {
+            var hotWaterReading = await _context.HotWaterReadings.FindAsync(id);
+
+            if (hotWaterReading == null)
+            {
+                return NotFound();
+            }
+
+            return hotWaterReading;
+        }
+
+        // PUT: api/HotWaterReadings/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHotWaterReading(int id, HotWaterReading hotWaterReading)
+        {
+            if (id != hotWaterReading.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(hotWaterReading).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HotWaterReadingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/HotWaterReadings
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<HotWaterReading>> PostHotWaterReading(HotWaterReading hotWaterReading)
+        {
+            _context.HotWaterReadings.Add(hotWaterReading);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetHotWaterReading", new { id = hotWaterReading.Id }, hotWaterReading);
+        }
+
+        // DELETE: api/HotWaterReadings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHotWaterReading(int id)
+        {
+            var hotWaterReading = await _context.HotWaterReadings.FindAsync(id);
+            if (hotWaterReading == null)
+            {
+                return NotFound();
+            }
+
+            _context.HotWaterReadings.Remove(hotWaterReading);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool HotWaterReadingExists(int id)
+        {
+            return _context.HotWaterReadings.Any(e => e.Id == id);
+        }
+    }
+}
