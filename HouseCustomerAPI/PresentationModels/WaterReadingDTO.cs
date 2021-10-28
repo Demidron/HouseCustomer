76087ba78dc9fcff2ae0405c0bac40d1f1924da2
Demidron <andrew.demidyuk@gmail.com>
2021-10-28 12:29:16 +0300

    Add new dto in api

---
+++ HouseCustomerAPI/PresentationModels/WaterReadingDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseCustomerAPI.PresentationModels
+{
+    public class WaterReadingDTO
+    {
+        public int Id { get; set; }
+        public int ApartmentId { get; set; }
+        public int ConsumerWriterId { get; set; }
+        public int? LastReadings { get; set; }
+        public DateTime? LastReadingsDate { get; set; }
+        public int CurrentReadings { get; set; }
+        public DateTime CurrentReadingsDate { get; set; }
+    }
+}
