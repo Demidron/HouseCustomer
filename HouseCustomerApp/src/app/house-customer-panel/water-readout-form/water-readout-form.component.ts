d9de798a5b20f03bcdda4c5f683607912801918a
Demidron <andrew.demidyuk@gmail.com>
2021-10-28 12:28:54 +0300

    Change water readout component

--- HouseCustomerApp/src/app/house-customer-panel/water-readout-form/water-readout-form.component.ts
+++ HouseCustomerApp/src/app/house-customer-panel/water-readout-form/water-readout-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { AbstractControl, FormControl, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 import { NgbDatepicker, NgbDatepickerI18n } from '@ng-bootstrap/ng-bootstrap';
+import { ConsumerService } from 'src/app/services/consumer.service';
+import { WaterReadoutService } from 'src/app/services/water-readout.service';
+import { ValidateWaterReading } from 'src/app/validators/water-reading-not-less.validator';
 
 @Component({
   selector: 'app-water-readout-form',
@@ -9,34 +12,54 @@ import { NgbDatepicker, NgbDatepickerI18n } from '@ng-bootstrap/ng-bootstrap';
 })
 export class WaterReadoutFormComponent implements OnInit {
 
+  // private date=new Date();
   private monthNames:string[] = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-  ];
+  "July", "August", "September", "October", "November", "December"];
   public currentMonth:string ="";
-  coldWaterReading=new FormControl(
-    // null,
-    // [
-    //   // Validators.required,
-    //   (ac:AbstractControl)=>{ ac.value; 
-    //     var str="";
-    //     for (let index = 0; index < ac.value.length; index++) {
-    //       if(ac.value[index]!="0") console.log(str+=ac.value[index]);
-    //     }
-    //     return null;
-    //   },
-    //   Validators.minLength(6),
-    //   // Validators.maxLength(12)
-    //   // NonZero.validate
-    // ]
-  );
+  public waterForm: FormGroup =new FormGroup({
+    // FormControl({value: 'Nancy', disabled: true}, Validators.required)
+    'hotWaterReading':new FormControl(
+     null, // {value: '', disabled:this.waterReadoutService.hotWaterReading.currentReadingsDate.getMonth>=new Date().getMonth}
+      [
+        
+        (ac:AbstractControl)=>{ 
+          let str=ac.value as string;
+          if(str&&str.length!=6)
+          {
+            str=('000000' + str).slice(-6)
+            ac.setValue(str);
+          }
+          return null;
+        },
+        ValidateWaterReading.createValidator(this.waterReadoutService),
+        Validators.required,
+        // NonZero.validate
+      ]),
+  })
+ 
   // @ViewChild(NgbDatepicker, {static: true}) datepicker: NgbDatepicker;
 
-  constructor(public i18n: NgbDatepickerI18n) {}
+  constructor(public i18n: NgbDatepickerI18n,public waterReadoutService:WaterReadoutService,public consumerService:ConsumerService) {}
 
   ngOnInit(): void {
-    const d = new Date();
-    this.currentMonth=this.monthNames[d.getMonth()]
+    this.currentMonth=this.monthNames[new Date().getMonth()];
   }
-  
- 
+  get diff() { return this.waterReadoutService.hotWaterReading.lastReadings? parseInt(this.waterForm.value["hotWaterReading"])-this.waterReadoutService.hotWaterReading.currentReadings : ""}
+
+ inputHotWaterIsActive() {return this.waterReadoutService.hotWaterReading.currentReadingsDate.getMonth==new Date().getMonth}
+  get hotWaterReading(){return this.waterForm.controls["hotWaterReading"]}
+
+  onSubmit(){
+
+    if(this.consumerService.authorizedConsumer){
+      this.waterReadoutService.saveNewHotWaterReading({
+        apartmentId:this.consumerService.apartments[0].apartmentId,
+        consumerWriterId:this.consumerService.authorizedConsumer?.id,
+        lastReadings:this.waterReadoutService.hotWaterReading.currentReadings,
+        lastReadingsDate:this.waterReadoutService.hotWaterReading.currentReadingsDate,
+        currentReadings:parseInt(this.waterForm.value["hotWaterReading"]),
+        currentReadingsDate:new Date()
+      })
+    }
+  }
 }
